952b3a0e6ce1ab3ffb300d58955caa1784279edb
aidarmukhoiarov <https://github.com/tenezma@gmail.com/helloworld.git>
2025-11-02 16:06:38 +0300

    commit 2

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,7 @@
 public class Book {
     private final String nameBook;
     private int publishYear;
-    Author author;
+    private Author author;
 
     public Book(String nameBook, int year, String author) {
         this.nameBook = nameBook;
@@ -23,9 +23,5 @@ public class Book {
 
     public Author getAuthor() {
         return author;
-    }
-
-    public void setAuthor(Author author) {
-        this.author = author;
     }
 }
